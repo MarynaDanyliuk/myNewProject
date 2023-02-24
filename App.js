39760506743a3325a807b619f75f888f82114f40
Mary Danyliuk <marydanyliuk@gmail.com>
2023-02-24 22:33:33 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: "green", fontSize: "30px" }}>
+        Open up App.js to start working on your app! Mary is great!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
