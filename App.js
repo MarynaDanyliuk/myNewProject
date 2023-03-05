1277f1210cad552342c1c5ba2dd18b10dc4f1d49
Mary Danyliuk <marydanyliuk@gmail.com>
2023-03-05 17:30:26 +0200

    add font

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
@@ -15,11 +15,11 @@ import {
 
 import * as Font from "expo-font";
 
-import AppLoading from "expo-app-loading";
+// import AppLoading from "expo-app-loading";
 
 // import { AppLoading } from "expo";
 
-// import * as SplashScreen from "expo-splash-screen";
+import * as SplashScreen from "expo-splash-screen";
 
 // import { RegistrationScreen } from "./Screens/RegistrationScreen";
 // import { LoginScreen } from "./Screens/LoginScreen";
@@ -48,11 +48,43 @@ export default function App() {
   const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await loadApplication();
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setIsReady(true);
+      }
+    }
+
+    prepare();
+    // ____________________________
     // async function dismissSplash() {
     //   await SplashScreen.hideAsync();
     // }
     // dismissSplash();
   });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
 
   const keyboardHide = () => {
     setIsShownKeyboard(false);
@@ -60,19 +92,19 @@ export default function App() {
     Keyboard.dismiss();
   };
 
-  if (!isReady) {
-    return (
-      <AppLoading
-        startAsync={loadApplication}
-        onFinish={() => setIsReady(true)}
-        onError={console.warn}
-      />
-    );
-  }
+  // if (!isReady) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={loadApplication}
+  //       onFinish={() => setIsReady(true)}
+  //       onError={console.warn}
+  //     />
+  //   );
+  // }
 
   // const { login, email, password } = state;
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <TouchableWithoutFeedback onPress={keyboardHide}>
         <ImageBackground
           source={require("./assets/images/background.jpg")}
